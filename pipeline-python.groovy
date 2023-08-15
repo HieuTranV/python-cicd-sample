7e2760026e4155bdd26fbd6cf9633314af1434b1
root <hieutranvan25@gmail.com>
2023-08-15 13:39:21 +0700

    update

--- pipeline-python.groovy
+++ pipeline-python.groovy
@@ -16,7 +16,7 @@ pipeline {
     stage('Deployment') {
       steps {
         sshagent(['ansible_demo']) {
-          sh 'ssh -o StrictHostKeyChecking=no root@10.1.1.210 export env'
+          sh 'ssh -o StrictHostKeyChecking=no root@10.1.1.210 env'
           sh 'ssh -o StrictHostKeyChecking=no root@10.1.1.210 pwd'
         }
       }
